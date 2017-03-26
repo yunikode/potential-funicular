8624969f3f0ca90162dbed2b36533609ec299274
zee-german <franz.lutzenberger@gmail.com>
2017-03-26 21:13:23 +0200

    code highlighting with highlight.js and atom-one-light theme

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
 const { dialog } = require('electron').remote
 const fs = require('fs')
 const marked = require('marked')
+const highlightjs = require('highlight.js')
+marked.setOptions({
+  smartypants: true
+  // highlight: function (code) {
+  //   return require('highlight.js').highlightAuto(code).value
+  // }
+})
+const renderer = new marked.Renderer()
+renderer.table = (header, body) => {
+  return '<table class="ui basic table"> <thead>' + header + '</thead> <tbody>' + body + '</tbody> </table>'
+}
+renderer.code = (code, language) => {
+  const validLang = !!(language && highlightjs.getLanguage(language))
+  const highlighted = validLang ? highlightjs.highlight(language, code).value : code
+  return `<div class="ui segment"><pre><code class="hljs ${language}">${highlighted}</code></pre></div>`
+}
 
 document.getElementById('openFile').addEventListener('click', function () {
   dialog.showOpenDialog(function (files) {
@@ -9,7 +25,7 @@ document.getElementById('openFile').addEventListener('click', function () {
     } else {
       fs.readFile(files[0], 'utf-8', (err, data) => {
         if (err) throw err
-        document.getElementById('main').innerHTML = marked(data)
+        document.getElementById('main').innerHTML = marked(data, { renderer })
       })
     }
   })
